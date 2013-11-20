a71fbf7fdc925e58a0883c56134ee076cdcf0e9a
Marcella <marcellahastings@gmail.com>
2013-11-20 14:36:57 -0500

    tensor documentation and simplification

--- Tensor.hs
+++ Tensor.hs
@@ -11,6 +11,8 @@ data Tensor = Tensor { a :: Float, b :: Float, d :: Float }
 trivial :: Tensor
 trivial = Tensor 0 0 0
 
+-- Creating Tensors from various types of constraints
+
 fromRTheta :: Float -> Float -> Tensor
 fromRTheta r t = Tensor (r*cos(2*t)) (r*sin(2*t)) (-1*r*cos(2*t))
 
@@ -19,10 +21,7 @@ fromXY x y = let xy = -2 * x * y
                  diffSquares = y * y - x * x
              in Tensor diffSquares xy (-1 * diffSquares)
 
-isReal :: Tensor -> Bool
-isReal (Tensor a1 b1 c1) = 
-  let isreal x = not (Prelude.isNaN x || isInfinite x) 
-  in isreal a1 && isreal b1 && isreal c1
+-- Basic Tensor operations
 
 add :: Tensor -> Tensor -> Tensor
 add (Tensor a1 b1 d1) (Tensor a2 b2 d2) =
@@ -34,24 +33,27 @@ sum = foldl add trivial
 scalarTimes :: Float -> Tensor-> Tensor
 scalarTimes c (Tensor a1 b1 d1) = Tensor (c*a1) (c*b1) (c*d1)
 
-eigenvalue :: Float -> Float -> Float -> Float
-eigenvalue _ b _ =
-  let ev = b -- (4*b*b + (a - d)*(a - d))
-  in if isNaN ev then error "broken" else ev
+isReal :: Tensor -> Bool
+isReal (Tensor a b c) = 
+  let isreal x = not (Prelude.isNaN x || isInfinite x) 
+  in isreal a && isreal b && isreal c
 
+-- Calculating eigenvalues and vectors
+
+-- returns a tuple of the (larger, smaller) eigenvectors of a tensor
+eigenvalues :: Tensor -> (Float, Float)
+eigenvalues (Tensor a b d) = 
+  let evals f = (f (a + d) (sqrt (4*b*b + (a - d)*(a - d)))) / 2
+  in  (evals (+), evals (-))
+
+-- returns a tuple of the (major, minor) eigenvectors for a tensor
 eigenvectors :: Tensor -> (V2.Vector2, V2.Vector2)
-eigenvectors (Tensor a1 0.0 d1) = 
-  if a1 > d1 then (V2.Vector2 1 0, V2.Vector2 0 1)
+eigenvectors (Tensor a 0.0 d) = 
+  if a > d then (V2.Vector2 1 0, V2.Vector2 0 1)
              else (V2.Vector2 0 1, V2.Vector2 1 0)
-eigenvectors (Tensor a1 b1 d1) =
-    let eval1 = ((a1 + d1) + sqrt (4*b1*b1 + (a1 - d1)*(a1 - d1)))/2
-        eval2 = ((a1 + d1) - sqrt (4*b1*b1 + (a1 - d1)*(a1 - d1)))/2
-        evec1 = V2.Vector2 1 ((eval1 - a1)/b1)
-        evec2 = V2.Vector2 1 ((eval2 - a1)/b1)
-    --there are still unreal values somewhere
-    in (evec1, evec2)
-    --in (V2.removeUnreal evec1, V2.removeUnreal evec2)
-  {--
-  in if V2.isReal evec1 && V2.isReal evec2 then (evec1, evec2)
-                                           else (V2.zero, V2.zero)
-  --}
+eigenvectors (Tensor a b d) =
+    let (eval1, eval2) = eigenvalues (Tensor a b d)
+        mkEvec eval    = V2.Vector2 1 ((eval - a) / b)
+    in (mkEvec eval1, mkEvec eval2)
+
+
